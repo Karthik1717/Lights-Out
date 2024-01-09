ed3510efc16857ea10c388d133b4025eb822ef98
Karthik1717 <karthikkamath98@yahoo.com>
2024-01-09 22:35:02 +0530

    Fixed build errors

---
+++ build/precache-manifest.3d1e037d68506393aec2b589326ff769.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bbc899353a992318364",
+    "url": "/static/css/main.60a4f2a2.chunk.css"
+  },
+  {
+    "revision": "9bbc899353a992318364",
+    "url": "/static/js/main.9bbc8993.chunk.js"
+  },
+  {
+    "revision": "b43a7eab4c27c21e54e5",
+    "url": "/static/js/1.b43a7eab.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a5754026da433f9a99178f58f1c030cb",
+    "url": "/index.html"
+  }
+];
